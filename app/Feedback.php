8d8a8383e2686c9c3afadae2ffcc2e9f84711a76
Usmon <usmonzaripov@gmail.com>
2020-03-25 23:15:33 +0500

    Migration for Feedback model status and scope unreaded

--- app/Feedback.php
+++ app/Feedback.php
@@ -6,11 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
+    const STATUS_UNREAD = 0;
+    const STATUS_READED = 1;
+
     public $table = 'feedbacks';
     public $fillable = [
         'name',
         'email',
         'subject',
-        'message'
+        'message',
+        'status'
     ];
+
+    public function scopeUnreaded()
+    {
+        return $this->where('status', '=', self::STATUS_UNREAD)->latest();
+    }
 }
